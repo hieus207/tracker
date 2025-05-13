90fb45dd4c5eb944f9dba7e4d8a5adfa1b7e137b
hieus207 <hieus207@gmail.com>
2025-05-13 14:00:41 +0700

    add tele 7

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
-    <script src="https://telegram.org/js/telegram-web-app.js"></script>
     <App />
   </BrowserRouter>
   // </React.StrictMode>
